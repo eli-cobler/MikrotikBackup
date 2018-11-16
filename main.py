b8191bd1d5ef1292d97c8c230ac5710e5ab5e2e5
Eli Cobler <ecobler11@gmail.com>
2018-11-16 16:03:29 -0600

    Updating comments

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        name = request.form['name']
+    if request.method == 'POST':                    
+        name = request.form['name']                                 # grabbing needed info from user
         router_ip = request.form['router_ip']
         username = request.form['username']
         password = request.form['password']
 
-        update.db(name, router_ip, username, password)
+        update.db(name, router_ip, username, password)              # writing new line to db file
 
-    return render_template('index.html') 
+    return render_template('index.html')                            # reloading page
 
 app.run(debug=True, host='0.0.0.0')
