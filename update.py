44bafa00341d13f54a20ac3c5544a53262be932e
Eli Cobler <ecobler11@gmail.com>
2018-11-16 16:24:01 -0600

    Updating path for remote server

--- update.py
+++ update.py
@@ -4,5 +4,5 @@ def db(name, router_ip, username, password):
     # name of location:router_ip:routeros:username:password:enable_password
     # SPARK Office:10.10.12.1:routeros:admin:SuperSpark1@3:enable_password
     # /home/oxidized/.config/oxidized/router.db
-    with open("/Users/coblere/Documents/GitHub/MikrotikBackup/router.db", 'a') as f: 
+    with open("/home/oxidized/.config/oxidized/router.db", 'a') as f: 
         f.write("{}:{}:routeros:{}:{}:enable_password\n".format(name, router_ip, username, password))  # writes new router to database file   
