3c05a21200f55cd1ba5ad9e25874427a52b19e98
Eli Cobler <ecobler11@gmail.com>
2019-02-19 17:45:30 -0600

    Update testBackup.py

--- testBackup.py
+++ testBackup.py
@@ -75,7 +75,7 @@ def create(router_name, router_ip, username, password):
         print("Unexpected Error, no backup was grabbed.")
         status = "Unexpected Error"
 
-    todays_date = date.today().strftime('%m-%d-%Y')
+    todays_date = datetime.datetime.today().strftime('%m-%d-%Y')
     database.update(router_name,router_ip,username,password,router_name,status, todays_date)
 
 router_name = input("What is the router_name? ")
