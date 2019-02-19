1bb01320703ad854d2465749e3bcbcd1548345b4
Eli Cobler <ecobler11@gmail.com>
2019-02-19 17:32:21 -0600

    Update testBackup.py

--- testBackup.py
+++ testBackup.py
@@ -3,12 +3,13 @@ from datetime import date
 
 def create(router_name, router_ip, username, password):
     # getting current date and time for naming backup file
-    date_time = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
-    filename = date_time + ".backup"
+    date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
+    filename = date + ".backup"
+    export_name = date + ".rsc"
 
     try:
         print("Generating backup for {}...".format(router_name))
-        # sshing into router to create .backup file
+        # sshing into router to create .backup and export config file
         ssh = paramiko.SSHClient()
         print('Created client')
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -16,27 +17,34 @@ def create(router_name, router_ip, username, password):
         print('Connecting to router...')
         ssh.connect(router_ip, username=username, password=password)
         print('Running backup command')
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("/system backup save name={}".format(date_time))
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("/system backup save name={}".format(date))
+        print('Running export command')
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("export file={}".format(date))
         print("Closing connection")
         ssh.close()
 
-        # sftping backup file from router to server
+        # sftping backup and exported config file from router to server
         transport = paramiko.Transport((router_ip))
         print("Transport created")
         print("Attemption connection...")
         transport.connect(username = username, password = password)
         print("Sftp created")
         sftp = paramiko.SFTPClient.from_transport(transport)
-        print("Set remote and local path")
+        print("Set remote and local path for backup file...")
         remotepath = "/{}.backup".format(date)
         localpath = 'backups/{}/{}'.format(router_name,filename)
         print("Backup transfered")
+        sftp.get(remotepath, localpath)
+        print("Set remote and local path for exported config...")
+        remotepath = "/{}.rsc".format(date)
+        localpath = 'backups/{}/{}'.format(router_name,export_name)
+        print("Exported config transfered")
         sftp.get(remotepath, localpath)
         print("Closing sftp and transport")
         sftp.close()
         transport.close()
 
-        # sshing back into router to remove .backup file
+        # sshing back into router to remove .backup and exported config file
         ssh = paramiko.SSHClient()
         print('Created client')  
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -45,6 +53,8 @@ def create(router_name, router_ip, username, password):
         ssh.connect(router_ip, username=username, password=password)
         print('Running remove backup command')
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('/file remove "{}"'.format(filename))
+        print('Running remove export command')
+        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('/file remove "{}"'.format(export_name))
         print("Closing connection")    
         ssh.close()
         print("Backup for {} complete.".format(router_name))
@@ -61,6 +71,9 @@ def create(router_name, router_ip, username, password):
     except paramiko.ssh_exception.NoValidConnectionsError as err:
         print(err)
         status = err
+    except:
+        print("Unexpected Error, no backup was grabbed.")
+        status = "Unexpected Error"
 
     todays_date = date.today().strftime('%m-%d-%Y')
     database.update(router_name,router_ip,username,password,router_name,status, todays_date)
