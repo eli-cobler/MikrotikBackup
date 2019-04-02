b265f74f7960be2f17ef67bdb8b5b2414a842f37
Eli Cobler <ecobler11@gmail.com>
2019-04-01 22:07:27 -0500

    Update export.py

--- export.py
+++ export.py
@@ -6,7 +6,7 @@ router_ip = '66.76.252.34'
 date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
 export_name = date + ".rsc"
 #print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
-subprocess.run('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
+subprocess.run('ssh {}@{} export terse > {}'.format(username, router_ip, export_name), shell=True)
 
 # ssh {}@{} export terse > {}.format(username, router_ip, export_name) 
 
