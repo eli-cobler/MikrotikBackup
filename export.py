097a1640cf295caf53545c19daf96fdd9ba9b8b9
Eli Cobler <eli@sparkservices.com>
2019-04-02 19:50:34 -0500

    update to export.py

--- export.py
+++ export.py
@@ -2,7 +2,7 @@ import subprocess, datetime
 
 username = 'admin'
 password = 'Admin1@3'
-router_ip = '66.76.254.121'
+router_ip = '66.76.254.96'
 date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
 export_name = date + ".rsc"
 #print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
