81e4e0fc3a0e06d695005cd9df8401c498953f06
Eli Cobler <ecobler11@gmail.com>
2019-04-02 22:04:19 -0500

    Update export.py

--- export.py
+++ export.py
@@ -1,14 +1,20 @@
-import subprocess, datetime
+import subprocess, datetime, database
 
-username = 'admin'
-password = 'Admin1@3'
-router_ip = '66.76.254.96'
-date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
-export_name = date + ".rsc"
-#print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
-subprocess.run('ssh {}@{} export terse > backups/{}'.format(username, router_ip, export_name), shell=True)
+def config(router_name, username, router_ip):
+    date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
+    export_name = date + ".rsc"
+    #print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
+    subprocess.run('ssh {}@{} export terse > backups/{}/{}'.format(username, router_ip, router_name, export_name), shell=True)
 
-# ssh {}@{} export terse > {}.format(username, router_ip, export_name) 
+def run():
+    router_list = database.get()
+    routers = []
+    for item in router_list:
+        data = item.split(':')
+        data[0] = {'router_name': data[0], 'router_ip': data[1], 'username': data[2], 'password': data[3].replace('\n', '')}
+        routers.append(data[0])            
+    
+    for item in routers:
+         config(item['router_name'], item['username'], item['router_ip'])
 
-# ssh {}@{} export terse > {}.format(username, router_ip, export_name) 
-
+config('Aces', 'admin', '66.76.254.121')
