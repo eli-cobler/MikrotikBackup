fcf6102449dc7adaab75aaae61d3cf8f37f31361
Eli Cobler <ecobler11@gmail.com>
2019-04-02 22:10:21 -0500

    Update export.py

--- export.py
+++ export.py
@@ -1,10 +1,24 @@
-import subprocess, datetime, database
+import subprocess, datetime, database, sys
 
 def config(router_name, username, router_ip):
-    date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
-    export_name = date + ".rsc"
-    #print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
-    subprocess.run('ssh {}@{} export terse > backups/{}/{}'.format(username, router_ip, router_name, export_name), shell=True)
+    try:
+        date = datetime.datetime.today().strftime('%m-%d-%Y_%H:%M:%S')
+        export_name = date + ".rsc"
+        #print('ssh {}@{} export terse > {}'.format(username, router_ip, export_name))
+        subprocess.run('ssh {}@{} export terse > backups/{}/{}'.format(username, router_ip, router_name, export_name), shell=True)
+    except TimeoutError as err:
+        print(err)
+        config_status = err
+    except EOFError as err:
+        print(err)
+        config_status = err
+    except FileNotFoundError as err:
+        print(err)
+        config_status = err
+    except:
+        the_type, the_value, the_traceback = sys.exc_info()
+        print("{}\n{}".format(the_type, the_value))
+        config_status = the_value
 
 def run():
     router_list = database.get()
