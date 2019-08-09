1eb311b2ab19afbfcbe6cc953893a3388f95ec97
Eli Cobler <ecobler11@gmail.com>
2019-08-09 15:13:01 -0500

    Update get_router_version.py
    Added ability to log info to file to reference later.

--- get_router_version.py
+++ get_router_version.py
@@ -11,6 +11,8 @@
 
 import subprocess, database, os, logging, paramiko
 
+
+
 # log setup
 logging.basicConfig(filename='logs/get_router_version.log',
                     format='%(asctime)s %(levelname)s %(message)s',
@@ -19,53 +21,28 @@ logging.basicConfig(filename='logs/get_router_version.log',
                     level=logging.DEBUG)
 
 def get_info(router_name,router_ip, username):
-    print("Gathering info for {}...".format(router_name))
+    print('Gathering info for {}...'.format(router_name))
+    logging.info('Gathering info for {}...'.format(router_name))
     # sshing into router to get router OS verison
     print('Running system info command')
+    logging.info('Running system info command')
     router_info = subprocess.run('ssh {}@{} /system resource print'.format(username, 
                                                                     router_ip),
                                                                     shell=True,
                                                                     universal_newlines=True,
                                                                     stdout=subprocess.PIPE,
                                                                     stderr=subprocess.PIPE)
-    #system_info = stdout.read().decode('ascii').strip("\n")
-    print(router_info.stdout)
+    #print(router_info.stdout)
 
-'''def get_info(router_name,router_ip,username):
-    k = paramiko.RSAKey.from_private_key_file("id_rsa-2.pub")
-    c = paramiko.SSHClient()
-    c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print("connecting")
-    c.connect( hostname = router_ip, username = username, key_filename = k )
-    print("connected")
-    command = [ "/system resource print"]
-    print("Executing {}".format( command ))
-    stdin , stdout, stderr = c.exec_command(command)
-    print(stdout.read())
-    print( "Errors")
-    print(stderr.read())
-    c.close()'''
+    # paths to router info file 
+    filepath = 'router_info/{}/info.txt'.format(router_name)
+    with open(filepath, 'w') as f:
+        f.write(router_info.stdout)
+
+
 
 if __name__ == "__main__":
-    router_name = input("Router Name: ")
-    router_ip = input("Router IP: ")
-    username = input("Username: ")
+    router_name = input('Router Name: ')
+    router_ip = input('Router IP: ')
+    username = input('Username: ')
     get_info(router_name, router_ip, username)
-
-
-'''
-import paramiko
-k = paramiko.RSAKey.from_private_key_file("id_rsa-2.pub")
-c = paramiko.SSHClient()
-c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-print "connecting"
-c.connect( hostname = "www.acme.com", username = "ubuntu", pkey = k )
-print "connected"
-commands = [ "/home/ubuntu/firstscript.sh", "/home/ubuntu/secondscript.sh" ]
-for command in commands:
-	print "Executing {}".format( command )
-	stdin , stdout, stderr = c.exec_command(command)
-	print stdout.read()
-	print( "Errors")
-	print stderr.read()
-c.close()'''
