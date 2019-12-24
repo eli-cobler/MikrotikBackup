bc9029a5d945b1bd5391b8bfcc3da58edaa3d302
Eli Cobler <ecobler11@gmail.com>
2019-12-23 22:21:16 -0600

    Update test_backup_service.py
    Add init db function

--- services/test_backup_service.py
+++ services/test_backup_service.py
@@ -1,3 +1,5 @@
+import os
+from data import db_session
 from services import router_details_service
 from services import backup_service
 
@@ -6,6 +8,12 @@ router_ip = input("What is the router_ip? ")
 username = input("What is the username? ")
 password = input("What is the password? ")
 
+def init_db():
+    top_folder = os.path.dirname(__file__)
+    rel_file = os.path.join('..', 'db', 'mikrotikbackup.sqlite')
+    db_file = os.path.abspath(os.path.join(top_folder, rel_file))
+    db_session.global_init(db_file)
+
 def run():
     backup_service.create_backup(router_name, router_ip, username)
     backup_service.create_config(router_name, router_ip, username)
@@ -13,4 +21,5 @@ def run():
     router_details_service.parse_info(router_name)
 
 if __name__ == "__main__":
+    init_db()
     run()
