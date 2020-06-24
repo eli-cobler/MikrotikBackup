0d28fb5ed967fea669b58d17e5d6dea9868a8221
Eli Cobler <ecobler11@gmail.com>
2020-06-24 12:19:50 -0500

    Update home_views.py

--- MikrotikBackup/views/home_views.py
+++ MikrotikBackup/views/home_views.py
@@ -23,29 +23,29 @@ def setup_db():
     db_session.global_init(db_file)
 
 
-# ################### Return Files for viewing and downloading files #################################
-@blueprint.route('/', defaults={'req_path': ''})
-@blueprint.route('/<path:req_path>')
-def dir_listing(req_path):
-    BASE_DIR = os.getcwd()
-
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
-
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        return abort(404)
-
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
-
-    backup_folder = os.path.basename(req_path)
-
-    # Show directory contents
-    files = os.listdir(abs_path)
-    files.sort(reverse=True)
-    return render_template('home/files.html', files=files, backups=backup_folder)
+# # ################### Return Files for viewing and downloading files #################################
+# @blueprint.route('/', defaults={'req_path': ''})
+# @blueprint.route('/<path:req_path>')
+# def dir_listing(req_path):
+#     BASE_DIR = os.getcwd()
+#
+#     # Joining the base and the requested path
+#     abs_path = os.path.join(BASE_DIR, req_path)
+#
+#     # Return 404 if path doesn't exist
+#     if not os.path.exists(abs_path):
+#         return abort(404)
+#
+#     # Check if path is a file and serve
+#     if os.path.isfile(abs_path):
+#         return send_file(abs_path)
+#
+#     backup_folder = os.path.basename(req_path)
+#
+#     # Show directory contents
+#     files = os.listdir(abs_path)
+#     files.sort(reverse=True)
+#     return render_template('home/files.html', files=files, backups=backup_folder)
 
 
 @blueprint.route('/', methods=['GET', 'POST'])
