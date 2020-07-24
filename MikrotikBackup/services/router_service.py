f87e335f0e505cecc3764cfc12c724fbf6f64a82
Eli Cobler <ecobler11@gmail.com>
2020-07-24 10:44:06 -0500

    Another attempted at bug squashing.

--- MikrotikBackup/services/router_service.py
+++ MikrotikBackup/services/router_service.py
@@ -134,18 +134,18 @@ def get_router_details(router_name: str)-> Optional[Router]:
 
 def add_router(router_name,router_ip,username,password,ignore):
 
-    path = os.getcwd()
-    print(f'path: {path}')
+    path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../backups/'))
+    logging.info(f'Add router path set to: {path}')
     directory_exists = os.path.isdir(path + f'/MikrotikBackup/backups/{router_name}')
-    print(f"Checking if directory for {router_name} already exists.")
+    logging.info(f"Checking if directory for {router_name} already exists.")
 
     if directory_exists:
-        print("The directory did already exist.")
+        logging.info("The directory did already exist.")
         return True
     else:
-        print("The directory didn't exist.")
+        logging.info("The directory didn't exist.")
         # writes new router to database file
-        print("Attempting to write new router to database.")
+        logging.info("Attempting to write new router to database.")
         r = Router()
 
         r.router_name = router_name
@@ -157,9 +157,9 @@ def add_router(router_name,router_ip,username,password,ignore):
         session = db_session.create_session()
         session.add(r)
         session.commit()
-        print("Database entries added successfully.")
+        logging.info("Database entries added successfully.")
 
-        print(f"Attempting to create backup folder for {router_name}")
+        logging.info(f"Attempting to create backup folder for {router_name}")
         try:
             os.mkdir(path + f'/MikrotikBackup/backups/{router_name}')
             f = open(f"router_info/{router_name}.txt", "w+")
@@ -181,14 +181,14 @@ def remove_router(router_name):
         .first()
     session.delete(router)
     session.commit()
-    print("Router successfully removed from database.")
+    logging.info("Router successfully removed from database.")
 
     print(f"Attempting to remove the backups for {router}.")
     try:
         # Gathering Backups path and removing it
         backup_path = os.path.abspath(os.path.join(os.path.dirname(__file__),f'../backups/{router_name}'))
         shutil.rmtree(backup_path)
-        print("Router backups successfully removed.")
+        logging.info("Router backups successfully removed.")
 
         # Gathering Router info.txt path and removing it
         router_info_path = os.path.abspath(os.path.join(os.path.dirname(__file__), f'../router_info/{router_name}.txt'))
@@ -201,10 +201,10 @@ def remove_router(router_name):
 def update_router(selected_router,router_name, router_ip, username, password, ignore):
     path = os.path.abspath(os.path.join(os.path.dirname(__file__),'../backups/'))
     logging.info(f'Path for update set to: {path}')
-    
+
     if router_name != selected_router:
-        print(f"Changing name of router from {selected_router} to {router_name}.")
-        print(f"Creating new backup directory for {router_name}")
+        logging.info(f"Changing name of router from {selected_router} to {router_name}.")
+        logging.info(f"Creating new backup directory for {router_name}")
 
         # creating new backup directory
         try:
@@ -221,17 +221,17 @@ def update_router(selected_router,router_name, router_ip, username, password, ig
             fromDirectory = path + f'/{selected_router}'
             toDirectory = path + f'/{router_name}'
             copy_tree(fromDirectory, toDirectory)
-            print("Files moved successfully.")
+            logging.info("Files moved successfully.")
         except:
             logging.error("There was a problem moving the backups. See the error below.")
             logging.error(sys.exc_info()[1])
 
-        print("Removing old backups directory.")
+        logging.info("Removing old backups directory.")
 
         # removing old backup directory
         try:
             shutil.rmtree(path + f'/{selected_router}')
-            print("Directory removed successfully.")
+            logging.info("Directory removed successfully.")
         except:
             logging.error("There was a problem removing the directory. See the error below.")
             logging.error(sys.exc_info()[1])
@@ -246,7 +246,7 @@ def update_router(selected_router,router_name, router_ip, username, password, ig
         #     logging.error(sys.exc_info()[1])
 
     # updating database values in sql database
-    print(f"Updating database values for {selected_router}.")
+    logging.info(f"Updating database values for {selected_router}.")
     session = db_session.create_session()
     r = session.query(Router).filter(Router.router_name == selected_router).one()
     r.router_name = router_name
@@ -255,7 +255,7 @@ def update_router(selected_router,router_name, router_ip, username, password, ig
     r.password = password
     r.ignore = ignore
     session.commit()
-    print("Database values updated successfully.")
+    logging.info("Database values updated successfully.")
 
 def find_router_by_name(router_name: str) -> Optional[Router]:
     session = db_session.create_session()
